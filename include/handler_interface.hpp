9c71aa723f43c3d4480cb8b4cc87041ecadecca0
waldemar <hellodoge@tuta.io>
2021-09-06 22:22:00 +0300

    add handling_error exception type

--- include/handler_interface.hpp
+++ include/handler_interface.hpp
@@ -9,6 +9,12 @@ namespace handler {
     // there is std::span in cpp20 standard, but it might be not supported yet
     using bytes_span = boost::beast::span<uint8_t>;
 
+    class handling_error : std::runtime_error {
+    public:
+        template <typename T>
+        explicit handling_error(T msg) : std::runtime_error(msg) {}
+    };
+
     class IHandler {
     public:
         virtual ~IHandler() = default;
