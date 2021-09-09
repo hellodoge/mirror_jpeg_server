0696443fa5a12b1d4f8982e8d8e900ae7ac4b80f
waldemar <hellodoge@tuta.io>
2021-09-09 16:49:10 +0300

    (main) graceful shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+#include <future>
+#include <csignal>
+#include <functional>
+
 #include "http_server.hpp"
 #include "mirror_jpeg_handler.hpp"
 #include "util/logger.hpp"
+
+void wait_for_stop_server_signal(const std::function<void(void)> &callback);
 
 int main() {
     server::ServerConfig config {
@@ -12,5 +18,31 @@ int main() {
     Logger logger {std::cout};
     handler::MirrorJPEGHandler handler {};
     server::HttpServer server {handler, config, logger};
-    server.run();
+
+
+    auto server_thread = std::async(std::launch::async, [&]() {
+	    server.run();
+    });
+
+	wait_for_stop_server_signal([&](){
+		server.stop();
+	});
+	server_thread.get();
+}
+
+void wait_for_stop_server_signal(const std::function<void(void)> &callback) {
+	sigset_t sigset;
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGTERM);
+	sigaddset(&sigset, SIGINT);
+
+	sigprocmask(SIG_BLOCK, &sigset, nullptr /* do not store prev values */);
+
+	int placeholder;
+	sigwait(&sigset, &placeholder);
+
+	callback();
+
+	// SIGTERM should be called after traffic switching
+	// then, after timeout, SIGKILL (for some corner cases)
 }
